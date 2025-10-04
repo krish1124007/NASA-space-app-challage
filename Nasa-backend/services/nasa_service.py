886f5eabad6a61c8111fbf608cb070535a6c50ad
parmar krish <parmarkrish005@gmail.com>
2025-10-04 09:19:35 +0530

    change bac-front

--- Nasa-backend/services/nasa_service.py
+++ Nasa-backend/services/nasa_service.py
@@ -1,11 +1,15 @@
 # backend/services/nasa_service.py
 import requests
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 NASA_API_KEY = os.getenv("NASA_API_KEY", "DEMO_KEY")
 BASE_URL = "https://api.nasa.gov/neo/rest/v1"
 
 def get_neo_feed(start_date, end_date):
+    print("nasa is",NASA_API_KEY)
     url = f"{BASE_URL}/feed?start_date={start_date}&end_date={end_date}&api_key={NASA_API_KEY}"
     r = requests.get(url)
     return r.json()
